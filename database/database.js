f720d233451e6156dd3f58b0a64baf3ae8fab6d8
alissonsouzaac <42471999+alissonsouzaac@users.noreply.github.com>
2020-05-26 02:46:18 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const connection = new Sequelize('guiapress','victordev','123456abc',{
+const connection = new Sequelize('guiapress','alissondev','123456abc',{
     host: 'mysql669.umbler.com',
     dialect: 'mysql',
     timezone: "-03:00"
